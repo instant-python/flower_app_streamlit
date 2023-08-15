dd8aabcbf9347cca117779b1696faafbc21f4710
John Hirosawa <john.hirosawa@gmail.com>
2023-08-14 20:57:45 -0400

    update

--- flower_app.py
+++ flower_app.py
@@ -8,4 +8,6 @@ add_auth(required=True)
 
 # only after authentication and subscription, the user will see below
 # email and subscription status is stored in session state.
-# st.write(f'Subscription Status: {st.session_state.user_subscribed}')
+st.write(f'Subscription Status: {st.session_state.user_subscribed}')
+st.write("Yay! You're all set and subscribed!")
+st.write(f'By the way, your email is: {st.session_state.email}')
